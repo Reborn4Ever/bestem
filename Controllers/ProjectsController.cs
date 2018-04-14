7acf347257bca80c4c7756c858af5d76acf65039
Reborn4Ever <adrian.nastase1207@gmail.com>
2018-04-14 14:52:11 +0300

    Project service added

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjectsReference;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NgProjectTrackerBEST.Services;
 
 namespace NgProjectTrackerBEST.Controllers
 {
@@ -11,12 +13,33 @@ namespace NgProjectTrackerBEST.Controllers
     [Route("api/Projects")]
     public class ProjectsController : Controller
     {
+
+        private const string NAV_PAGE_NAME = "Projects_ARGGOBEST";
+        private readonly NavServiceClientFactory _navServiceClientFactory;
+
+        public ProjectsController(NavServiceClientFactory navServiceClientFactory)
+        {
+            _navServiceClientFactory = navServiceClientFactory;
+        }
+
         // GET: api/Projects
-        [HttpGet]
+        [HttpGet("[action]/{searchName?}")]
+        public async Task<IActionResult> List(string searchName = null)
+        {
+
+            var serviceRef = _navServiceClientFactory.CreatePageClient<Projects_ARGGOBEST_PortClient, Projects_ARGGOBEST_Port>(NAV_PAGE_NAME,
+                (binding, address) => new Projects_ARGGOBEST_PortClient(binding, address));
+            Projects_ARGGOBEST_Filter[] filters = null;
+            if (!string.IsNullOrWhiteSpace(searchName))
+                filters = new[] { new Projects_ARGGOBEST_Filter { Field = Projects_ARGGOBEST_Fields.Project_Title, Criteria = "@*" + searchName + "*" } };
+            return Ok((await serviceRef.ReadMultipleAsync(filters, null, 0)).ReadMultiple_Result1);
+        }
+
+        /*[HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-        }
+        }*/
 
         // GET: api/Projects/5
         [HttpGet("{id}", Name = "Get")]
