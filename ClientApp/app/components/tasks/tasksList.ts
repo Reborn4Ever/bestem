34d088a336e8c43fe1b6c96d4f767367a8082094
Regina <soradia2000@gmail.com>
2018-04-14 21:34:38 +0300

    Show client name and project title

--- ClientApp/app/components/tasks/tasksList.ts
+++ ClientApp/app/components/tasks/tasksList.ts
@@ -9,26 +9,69 @@ import { Http } from "@angular/http";
 export class TasksListComponent {
 
     private tasks: Task[];
+    private projects: Project[];
+    private clients: Client[];
     searchTerm: string = "";
 
     refreshList() {
+        this.http.get(this.baseUrl + "api/ClientsController/List" + (this.searchTerm ? "/" + this.searchTerm : "")).subscribe(result => {
+            this.clients = result.json() as Client[];
+        }, error => console.error(error));
+
+        this.http.get(this.baseUrl + "api/ProjectsController/List" + (this.searchTerm ? "/" + this.searchTerm : "")).subscribe(result => {
+            this.projects = result.json() as Project[];
+        }, error => console.error(error));
+
         this.http.get(this.baseUrl + "api/TasksController/List" + (this.searchTerm ? "/" + this.searchTerm : "")).subscribe(result => {
             this.tasks = result.json() as Task[];
         }, error => console.error(error));
+        //console.log(this.projects);
+       // console.log(this.clients);
+       // console.log(this.tasks);
     }
 
     constructor(private readonly http: Http, @Inject("BASE_URL") private readonly baseUrl: string) {
         this.refreshList();
         
     }
+
+    getClientNameByID(id: string) {
+    //    console.log(id);
+        return this.clients.find(x => x.no == id);
+    }
+    getProjectNameByID(id: string) {
+        console.log(id);
+        return this.projects.find(x => x.project_ID == id);
+    }
+
 }
 
 export class Task {
-    user_ID: string;
+    owner_User_ID: string;
     project_ID: string;
     task_ID: string;
     task_Title: string;    
     task_Description: string;
     task_Status: string;
     priority: string;
+}
+
+export class Project {
+    key: string;
+    project_ID: string;
+    project_Title: string;
+    project_Description: string;
+    customer_ID: string;
+    customer_name: string;
+    pM_User_ID: string;
+    project_Status: string;
+}
+
+
+export class Client {
+    key: string;
+    no: string;
+    name: string;
+    address: string;
+    city: string;
 }
